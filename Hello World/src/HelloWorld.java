3843ede4a19e9cc6931cc37caf7da97a6dc22145
96650 <96650@LAPTOP-AD44979H>
2023-04-08 09:20:36 +0300

    jana

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Haneen Abdullah");
 		System.out.println("Amal Mohammed");
+		System.out.println("Jana Khalil");
 	}
 
 }
